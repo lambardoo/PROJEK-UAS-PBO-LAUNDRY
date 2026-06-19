44a4c7c0a02de74e84735c366def1214e6f007d0
lambardoo <petrickboen@gmail.com>
2026-06-20 03:00:32 +0700

    ..

--- models/database.py
+++ models/database.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def get_db_connection():
-    try:  # Exception Handling
+    try: 
         conn = mysql.connector.connect(
             host="localhost",
             user="root",
