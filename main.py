bfe2221dd3bcb1ef09aa668d246b3526fd505a96
lambardoo <petrickboen@gmail.com>
2026-06-21 23:38:03 +0700

    .

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ from routes.auth import (
     laporan_bp
 )
 
-# Objek Flask
 app = Flask(__name__)
 app.secret_key = 'kunci_rahasia_laundry_2026'
 
-# Registrasi Blueprint
 app.register_blueprint(auth_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(pelanggan_bp)
@@ -20,6 +18,5 @@ app.register_blueprint(layanan_bp)
 app.register_blueprint(transaksi_bp)
 app.register_blueprint(laporan_bp)
 
-# Menjalankan aplikasi
 if __name__ == '__main__':
     app.run(debug=True)
